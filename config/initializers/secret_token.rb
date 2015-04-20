cbb9533dea319e2a7060963c226c947e8342ee28
pascaline julien <pascaline.julien@gmail.com>
2015-04-20 11:26:38 +0200

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YellowPages::Application.config.secret_token = 'ac144726c684ffd43b39b06edac070a6d371be26d02f06c11ada8833f55f9a8b15a444ada7527bd2d595d504be9df3a85071b8488dab303029b51578aa985263'
